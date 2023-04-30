7fce5424a14818aa8c29f0e2c4ea3f5f7eeed80e
phalanx8 <mohamed.el.jemai@rwth-aachen.de>
2023-04-30 18:27:11 +0200

    Finish

--- unit8/install.sh
+++ unit8/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt update
-apt install -y python-opengl ffmpeg xvfb swig cmake
+apt install -y python-opengl ffmpeg xvfb swig cmake git-lfs
 pip insatll testresources
 pip install --upgrade pip
 pip install setuptools==65.5.0
--- unit8/unit8-part-sol.py
+++ unit8/unit8-part-sol.py
@@ -27,6 +27,7 @@ import imageio
 from wasabi import Printer
 msg = Printer()
 
+
 def parse_args():
     # fmt: off
     parser = argparse.ArgumentParser()
@@ -84,9 +85,9 @@ def parse_args():
         help="the maximum norm for the gradient clipping")
     parser.add_argument("--target-kl", type=float, default=None,
         help="the target KL divergence threshold")
-    
+
     # Adding HuggingFace argument
-    parser.add_argument("--repo-id", type=str, default="ThomasSimonini/ppo-CartPole-v1", help="id of the model repository from the Hugging Face Hub {username/repo_name}")
+    parser.add_argument("--repo-id", type=str, default="mojemai/ppo-CartPole-v1", help="id of the model repository from the Hugging Face Hub {username/repo_name}")
 
     args = parser.parse_args()
     args.batch_size = int(args.num_envs * args.num_steps)
@@ -94,85 +95,87 @@ def parse_args():
     # fmt: on
     return args
 
-def package_to_hub(repo_id, 
-                model,
-                hyperparameters,
-                eval_env,
-                video_fps=30,
-                commit_message="Push agent to the Hub",
-                token= None,
-                logs=None
-                ):
-  """
-  Evaluate, Generate a video and Upload a model to Hugging Face Hub.
-  This method does the complete pipeline:
-  - It evaluates the model
-  - It generates the model card
-  - It generates a replay video of the agent
-  - It pushes everything to the hub
-  :param repo_id: id of the model repository from the Hugging Face Hub
-  :param model: trained model
-  :param eval_env: environment used to evaluate the agent
-  :param fps: number of fps for rendering the video
-  :param commit_message: commit message
-  :param logs: directory on local machine of tensorboard logs you'd like to upload
-  """
-  msg.info(
+
+def package_to_hub(repo_id,
+                   model,
+                   hyperparameters,
+                   eval_env,
+                   video_fps=30,
+                   commit_message="Push agent to the Hub",
+                   token=None,
+                   logs=None
+                   ):
+    """
+    Evaluate, Generate a video and Upload a model to Hugging Face Hub.
+    This method does the complete pipeline:
+    - It evaluates the model
+    - It generates the model card
+    - It generates a replay video of the agent
+    - It pushes everything to the hub
+    :param repo_id: id of the model repository from the Hugging Face Hub
+    :param model: trained model
+    :param eval_env: environment used to evaluate the agent
+    :param fps: number of fps for rendering the video
+    :param commit_message: commit message
+    :param logs: directory on local machine of tensorboard logs you'd like to upload
+    """
+    msg.info(
         "This function will save, evaluate, generate a video of your agent, "
         "create a model card and push everything to the hub. "
         "It might take up to 1min. \n "
         "This is a work in progress: if you encounter a bug, please open an issue."
     )
-  # Step 1: Clone or create the repo
-  repo_url = HfApi().create_repo(
+    # Step 1: Clone or create the repo
+    repo_url = HfApi().create_repo(
         repo_id=repo_id,
         token=token,
         private=False,
         exist_ok=True,
     )
-  
-  with tempfile.TemporaryDirectory() as tmpdirname:
-    tmpdirname = Path(tmpdirname)
 
-    # Step 2: Save the model
-    torch.save(model.state_dict(), tmpdirname / "model.pt")
-  
-    # Step 3: Evaluate the model and build JSON
-    mean_reward, std_reward = _evaluate_agent(eval_env, 
-                                           10, 
-                                           model)
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        tmpdirname = Path(tmpdirname)
 
-    # First get datetime
-    eval_datetime = datetime.datetime.now()
-    eval_form_datetime = eval_datetime.isoformat()
+        # Step 2: Save the model
+        torch.save(model.state_dict(), tmpdirname / "model.pt")
 
-    evaluate_data = {
-        "env_id": hyperparameters.env_id, 
-        "mean_reward": mean_reward,
-        "std_reward": std_reward,
-        "n_evaluation_episodes": 10,
-        "eval_datetime": eval_form_datetime,
-    }
- 
-    # Write a JSON file
-    with open(tmpdirname / "results.json", "w") as outfile:
-      json.dump(evaluate_data, outfile)
+        # Step 3: Evaluate the model and build JSON
+        mean_reward, std_reward = _evaluate_agent(eval_env,
+                                                  10,
+                                                  model)
 
-    # Step 4: Generate a video
-    video_path =  tmpdirname / "replay.mp4"
-    record_video(eval_env, model, video_path, video_fps)
-  
-    # Step 5: Generate the model card
-    generated_model_card, metadata = _generate_model_card("PPO", hyperparameters.env_id, mean_reward, std_reward, hyperparameters)
-    _save_model_card(tmpdirname, generated_model_card, metadata)
+        # First get datetime
+        eval_datetime = datetime.datetime.now()
+        eval_form_datetime = eval_datetime.isoformat()
 
-    # Step 6: Add logs if needed
-    if logs:
-      _add_logdir(tmpdirname, Path(logs))
-  
-    msg.info(f"Pushing repo {repo_id} to the Hugging Face Hub")
-  
-    repo_url = upload_folder(
+        evaluate_data = {
+            "env_id": hyperparameters.env_id,
+            "mean_reward": mean_reward,
+            "std_reward": std_reward,
+            "n_evaluation_episodes": 10,
+            "eval_datetime": eval_form_datetime,
+        }
+
+        # Write a JSON file
+        with open(tmpdirname / "results.json", "w") as outfile:
+            json.dump(evaluate_data, outfile)
+
+        # Step 4: Generate a video
+        video_path = tmpdirname / "replay.mp4"
+        record_video(eval_env, model, video_path, video_fps)
+
+        # Step 5: Generate the model card
+        generated_model_card, metadata = _generate_model_card(
+            "PPO", hyperparameters.env_id, mean_reward, std_reward, hyperparameters)
+        _save_model_card(tmpdirname, generated_model_card, metadata)
+
+        # Step 6: Add logs if needed
+        if logs:
+            _add_logdir(tmpdirname, Path(logs))
+
+        msg.info(f"Pushing repo {repo_id} to the Hugging Face Hub")
+
+        repo_url = upload_folder(
             repo_id=repo_id,
             folder_path=tmpdirname,
             path_in_repo="",
@@ -180,74 +183,76 @@ def package_to_hub(repo_id,
             token=token,
         )
 
-    msg.info(f"Your model is pushed to the Hub. You can view your model here: {repo_url}")
-  return repo_url
+        msg.info(f"Your model is pushed to the Hub. You can view your model here: {repo_url}")
+    return repo_url
+
 
 def _evaluate_agent(env, n_eval_episodes, policy):
-  """
-  Evaluate the agent for ``n_eval_episodes`` episodes and returns average reward and std of reward.
-  :param env: The evaluation environment
-  :param n_eval_episodes: Number of episode to evaluate the agent
-  :param policy: The agent
-  """
-  episode_rewards = []
-  for episode in range(n_eval_episodes):
-    state = env.reset()
-    step = 0
-    done = False
-    total_rewards_ep = 0
-    
-    while done is False:
-      state = torch.Tensor(state).to(device)
-      action, _, _, _ = policy.get_action_and_value(state)
-      new_state, reward, done, info = env.step(action.cpu().numpy())
-      total_rewards_ep += reward    
-      if done:
-        break
-      state = new_state
-    episode_rewards.append(total_rewards_ep)
-  mean_reward = np.mean(episode_rewards)
-  std_reward = np.std(episode_rewards)
+    """
+    Evaluate the agent for ``n_eval_episodes`` episodes and returns average reward and std of reward.
+    :param env: The evaluation environment
+    :param n_eval_episodes: Number of episode to evaluate the agent
+    :param policy: The agent
+    """
+    episode_rewards = []
+    for episode in range(n_eval_episodes):
+        state = env.reset()
+        step = 0
+        done = False
+        total_rewards_ep = 0
 
-  return mean_reward, std_reward
+        while done is False:
+            state = torch.Tensor(state).to(device)
+            action, _, _, _ = policy.get_action_and_value(state)
+            new_state, reward, done, info = env.step(action.cpu().numpy())
+            total_rewards_ep += reward
+            if done:
+                break
+            state = new_state
+        episode_rewards.append(total_rewards_ep)
+    mean_reward = np.mean(episode_rewards)
+    std_reward = np.std(episode_rewards)
+
+    return mean_reward, std_reward
 
 
 def record_video(env, policy, out_directory, fps=30):
-  images = []  
-  done = False
-  state = env.reset()
-  img = env.render(mode='rgb_array')
-  images.append(img)
-  while not done:
-    state = torch.Tensor(state).to(device)
-    # Take the action (index) that have the maximum expected future reward given that state
-    action, _, _, _  = policy.get_action_and_value(state)
-    state, reward, done, info = env.step(action.cpu().numpy()) # We directly put next_state = state for recording logic
+    images = []
+    done = False
+    state = env.reset()
     img = env.render(mode='rgb_array')
     images.append(img)
-  imageio.mimsave(out_directory, [np.array(img) for i, img in enumerate(images)], fps=fps)
+    while not done:
+        state = torch.Tensor(state).to(device)
+        # Take the action (index) that have the maximum expected future reward given that state
+        action, _, _, _ = policy.get_action_and_value(state)
+        # We directly put next_state = state for recording logic
+        state, reward, done, info = env.step(action.cpu().numpy())
+        img = env.render(mode='rgb_array')
+        images.append(img)
+    imageio.mimsave(out_directory, [np.array(img) for i, img in enumerate(images)], fps=fps)
 
 
 def _generate_model_card(model_name, env_id, mean_reward, std_reward, hyperparameters):
-  """
-  Generate the model card for the Hub
-  :param model_name: name of the model
-  :env_id: name of the environment
-  :mean_reward: mean reward of the agent
-  :std_reward: standard deviation of the mean reward of the agent
-  :hyperparameters: training arguments
-  """
-  # Step 1: Select the tags
-  metadata = generate_metadata(model_name, env_id, mean_reward, std_reward)
+    """
+    Generate the model card for the Hub
+    :param model_name: name of the model
+    :env_id: name of the environment
+    :mean_reward: mean reward of the agent
+    :std_reward: standard deviation of the mean reward of the agent
+    :hyperparameters: training arguments
+    """
+    # Step 1: Select the tags
+    metadata = generate_metadata(model_name, env_id, mean_reward, std_reward)
 
-  # Transform the hyperparams namespace to string
-  converted_dict = vars(hyperparameters)
-  converted_str = str(converted_dict)
-  converted_str = converted_str.split(", ")
-  converted_str = '\n'.join(converted_str)
- 
-  # Step 2: Generate the model card
-  model_card = f"""
+    # Transform the hyperparams namespace to string
+    converted_dict = vars(hyperparameters)
+    converted_str = str(converted_dict)
+    converted_str = converted_str.split(", ")
+    converted_str = '\n'.join(converted_str)
+
+    # Step 2: Generate the model card
+    model_card = f"""
   # PPO Agent Playing {env_id}
 
   This is a trained model of a PPO agent playing {env_id}.
@@ -257,42 +262,44 @@ def _generate_model_card(model_name, env_id, mean_reward, std_reward, hyperparam
   {converted_str}
   ```
   """
-  return model_card, metadata
+    return model_card, metadata
+
 
 def generate_metadata(model_name, env_id, mean_reward, std_reward):
-  """
-  Define the tags for the model card
-  :param model_name: name of the model
-  :param env_id: name of the environment
-  :mean_reward: mean reward of the agent
-  :std_reward: standard deviation of the mean reward of the agent
-  """
-  metadata = {}
-  metadata["tags"] = [
+    """
+    Define the tags for the model card
+    :param model_name: name of the model
+    :param env_id: name of the environment
+    :mean_reward: mean reward of the agent
+    :std_reward: standard deviation of the mean reward of the agent
+    """
+    metadata = {}
+    metadata["tags"] = [
         env_id,
         "ppo",
         "deep-reinforcement-learning",
         "reinforcement-learning",
         "custom-implementation",
         "deep-rl-course"
-  ]
+    ]
 
-  # Add metrics
-  eval = metadata_eval_result(
-      model_pretty_name=model_name,
-      task_pretty_name="reinforcement-learning",
-      task_id="reinforcement-learning",
-      metrics_pretty_name="mean_reward",
-      metrics_id="mean_reward",
-      metrics_value=f"{mean_reward:.2f} +/- {std_reward:.2f}",
-      dataset_pretty_name=env_id,
-      dataset_id=env_id,
-  )
+    # Add metrics
+    eval = metadata_eval_result(
+        model_pretty_name=model_name,
+        task_pretty_name="reinforcement-learning",
+        task_id="reinforcement-learning",
+        metrics_pretty_name="mean_reward",
+        metrics_id="mean_reward",
+        metrics_value=f"{mean_reward:.2f} +/- {std_reward:.2f}",
+        dataset_pretty_name=env_id,
+        dataset_id=env_id,
+    )
 
-  # Merges both dictionaries
-  metadata = {**metadata, **eval}
+    # Merges both dictionaries
+    metadata = {**metadata, **eval}
 
-  return metadata
+    return metadata
+
 
 def _save_model_card(local_path, generated_model_card, metadata):
     """Saves a model card for the repository.
@@ -314,21 +321,23 @@ def _save_model_card(local_path, generated_model_card, metadata):
     # Save our metrics to Readme metadata
     metadata_save(readme_path, metadata)
 
-def _add_logdir(local_path: Path, logdir: Path):
-  """Adds a logdir to the repository.
-  :param local_path: repository directory
-  :param logdir: logdir directory
-  """
-  if logdir.exists() and logdir.is_dir():
-    # Add the logdir to the repository under new dir called logs
-    repo_logdir = local_path / "logs"
-    
-    # Delete current logs if they exist
-    if repo_logdir.exists():
-      shutil.rmtree(repo_logdir)
 
-    # Copy logdir into repo logdir
-    shutil.copytree(logdir, repo_logdir)
+def _add_logdir(local_path: Path, logdir: Path):
+    """Adds a logdir to the repository.
+    :param local_path: repository directory
+    :param logdir: logdir directory
+    """
+    if logdir.exists() and logdir.is_dir():
+        # Add the logdir to the repository under new dir called logs
+        repo_logdir = local_path / "logs"
+
+        # Delete current logs if they exist
+        if repo_logdir.exists():
+            shutil.rmtree(repo_logdir)
+
+        # Copy logdir into repo logdir
+        shutil.copytree(logdir, repo_logdir)
+
 
 def make_env(env_id, seed, idx, capture_video, run_name):
     def thunk():
@@ -577,10 +586,9 @@ if __name__ == "__main__":
     # Create the evaluation environment
     eval_env = gym.make(args.env_id)
 
-    package_to_hub(repo_id = args.repo_id,
-                model = agent, # The model we want to save
-                hyperparameters = args,
-                eval_env = gym.make(args.env_id),
-                logs= f"runs/{run_name}",
-                )
-    
+    package_to_hub(repo_id=args.repo_id,
+                   model=agent,  # The model we want to save
+                   hyperparameters=args,
+                   eval_env=gym.make(args.env_id),
+                   logs=f"runs/{run_name}",
+                   )
