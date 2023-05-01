b8cd1f3c23e77764b2c1f2591b434a3f24cd9949
phalanx8 <mohamed.el.jemai@rwth-aachen.de>
2023-05-01 09:46:39 +0200

    Added Part2

---
+++ unit8/install-part2.sh
@@ -0,0 +1,7 @@
+apt install build-essential zlib1g-dev libsdl2-dev libjpeg-dev \
+nasm tar libbz2-dev libgtk2.0-dev cmake git libfluidsynth-dev libgme-dev \
+libopenal-dev timidity libwildmidi-dev unzip ffmpeg
+
+apt install libboost-all-dev
+
+apt install liblua5.1-dev
